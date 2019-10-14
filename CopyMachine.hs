51d26155fabd3a797d3422878211d3c335ffd5f6
skaer2 <max.bog200174@gmail.com>
2019-10-14 20:21:54 +0300

    Add files via upload
    Unfinished multicopy function

--- CopyMachine.hs
+++ CopyMachine.hs
@@ -5,24 +5,56 @@ import System.IO
 import System.IO.Error
 
 main = do
-    (fileToCopy:destination:depth:xs) <- getArgs
-    copyFiles fileToCopy (read depth) destination 
+    (fileToCopy:destination:depth:numberOfCopies:xs) <- getArgs
+    initCopyMachine fileToCopy (read depth) destination  (read numberOfCopies)
+
+initCopyMachine :: FilePath -> Int -> FilePath -> Int -> IO ()
+initCopyMachine fileToCopy depth destination 0 = copyFiles fileToCopy depth destination
+initCopyMachine fileToCopy depth destination numberOfCopies = catchIOError (copyFiles fileToCopy depth destination) (handler fileToCopy destination)
+
 
 copyFiles :: FilePath -> Int -> FilePath -> IO ()
-copyFiles fileToCopy depth destination = catchIOError (toTry fileToCopy depth destination) (handler fileToCopy destination)
+copyFiles fileToCopy depth destination = catchIOError (copyFiles_ fileToCopy depth destination) (handler fileToCopy destination)
+    where   copyFiles_ :: FilePath -> Int -> FilePath -> IO ()
+            copyFiles_ fileToCopy depth destination = do 
+                absoluteDestination <- makeAbsolute destination
+                destinationContents <- listDirectory absoluteDestination
+                let destinationFolders = checkDirs destinationContents
+                let fileName = takeFileName fileToCopy
+                copyFile fileToCopy $ absoluteDestination </> fileName
+                putStrLn $ "copied " ++ fileName ++ " to " ++ absoluteDestination
+                let dirPath = absoluteDestination ++ "/"
+                if (destinationFolders == Nothing) || ( depth < 1)
+                    then return ()
+                    else let (Just dirs) = destinationFolders in mapM_ (copyFiles fileToCopy (depth - 1)) (map (dirPath ++) dirs) 
 
-toTry :: FilePath -> Int -> FilePath -> IO ()
-toTry fileToCopy depth destination = do 
+                    
+copyFilesMultiCopy :: FilePath -> Int -> FilePath -> Int -> IO ()
+copyFilesMultiCopy fileToCopy depth destination numberOfCopies = catchIOError (copyFiles_ fileToCopy depth destination) (handler fileToCopy destination)
+    where   copyFilesMultiCopy_ :: FilePath -> Int -> FilePath -> Int -> IO ()
+            copyFilesMultiCopy_ fileToCopy depth destination numberOfCopies = do 
+                absoluteDestination <- makeAbsolute destination
+                destinationContents <- listDirectory absoluteDestination
+                let destinationFolders = checkDirs destinationContents
+                let fileName = takeFileName fileToCopy
+                copyFile fileToCopy $ absoluteDestination </> fileName
+                putStrLn $ "copied " ++ fileName ++ " to " ++ absoluteDestination
+                let dirPath = absoluteDestination ++ "/"
+                if (destinationFolders == Nothing) || ( depth < 1)
+                    then return ()
+                    else let (Just dirs) = destinationFolders in mapM_ (copyFilesMultiCopy fileToCopy (depth - 1) numberOfCopies) (map (dirPath ++) dirs) 
+
+--fileAlreadyExists <- doesFileExist fileToCopy
+        --if fileAlreadyExists 
+            --then copyFile fileToCopy $ dir </> $ fileName ++
+
+multiCopy :: FilePath -> FilePath -> Int -> IO ()
+multiCopy fileToCopy destination numberOfCopies 
+multiCopy fileToCopy destination numberOfCopies = do
     absoluteDestination <- makeAbsolute destination
-    destinationContents <- listDirectory absoluteDestination
-    let destinationFolders = checkDirs destinationContents
-    let fileName = takeFileName fileToCopy
-    copyFile fileToCopy $ absoluteDestination </> fileName
-    putStrLn $ "copied " ++ fileName ++ " to " ++ absoluteDestination
-    let dirPath = absoluteDestination ++ "/"
-    if (destinationFolders == Nothing) || ( depth < 1)
-        then return ()
-        else let (Just dirs) = destinationFolders in mapM_ (copyFiles fileToCopy (depth - 1)) (map (dirPath ++) dirs) 
+    fileAlreadyExists <- doesFileExist fileToCopy
+        if fileAlreadyExists 
+            then copyFile fileToCopy $ absoluteDestination </> $ fileName ++ numberOfCopies
 
 handler :: FilePath -> FilePath -> IOError -> IO ()
 handler _ _ e = putStrLn "\nAn exception!"
