5dd83b22d67b8acd1a2feec58dde04495ec8cb2a
skaer2 <max.bog200174@gmail.com>
2019-10-14 20:19:42 +0300

    Add files via upload
    Added depth argument

--- CopyMachine.hs
+++ CopyMachine.hs
@@ -5,25 +5,24 @@ import System.IO
 import System.IO.Error
 
 main = do
-    (fileToCopy:destination:xs) <- getArgs
-    copyFiles fileToCopy destination 
+    (fileToCopy:destination:depth:xs) <- getArgs
+    copyFiles fileToCopy (read depth) destination 
 
-copyFiles :: FilePath -> FilePath -> IO ()
-copyFiles fileToCopy destination = catchIOError (toTry fileToCopy destination) (handler fileToCopy destination)
+copyFiles :: FilePath -> Int -> FilePath -> IO ()
+copyFiles fileToCopy depth destination = catchIOError (toTry fileToCopy depth destination) (handler fileToCopy destination)
 
-toTry :: FilePath -> FilePath -> IO ()
-toTry fileToCopy destination = do 
-    dir <- makeAbsolute destination
-    thisDir <- listDirectory dir
-    let newDirs = checkDirs thisDir
+toTry :: FilePath -> Int -> FilePath -> IO ()
+toTry fileToCopy depth destination = do 
+    absoluteDestination <- makeAbsolute destination
+    destinationContents <- listDirectory absoluteDestination
+    let destinationFolders = checkDirs destinationContents
     let fileName = takeFileName fileToCopy
-    copyFile fileToCopy $ dir </> fileName
-    putStrLn $ "copied " ++ fileName ++ " to " ++ dir
-    let dirPath = dir ++ "/"
-    if newDirs == Nothing 
+    copyFile fileToCopy $ absoluteDestination </> fileName
+    putStrLn $ "copied " ++ fileName ++ " to " ++ absoluteDestination
+    let dirPath = absoluteDestination ++ "/"
+    if (destinationFolders == Nothing) || ( depth < 1)
         then return ()
-        else let (Just dirs) = newDirs in mapM_ (copyFiles fileToCopy) (map (dirPath ++) dirs) 
-
+        else let (Just dirs) = destinationFolders in mapM_ (copyFiles fileToCopy (depth - 1)) (map (dirPath ++) dirs) 
 
 handler :: FilePath -> FilePath -> IOError -> IO ()
 handler _ _ e = putStrLn "\nAn exception!"
